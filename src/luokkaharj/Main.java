6634d40ddf503b12b1786574de45d6352fefcaed
miropat <93606923+miropat@users.noreply.github.com>
2022-12-07 12:27:12 +0200

    kolmas commit

--- src/luokkaharj/Main.java
+++ src/luokkaharj/Main.java
@@ -5,7 +5,34 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		//olion luominen
+		
+		Televisio tv1 = new Televisio();
+		
+		//Annetaan arvot
+		tv1.merkki = "Samsung";
+		tv1.malli = "Q60B 4K QLED";
+		tv1.koko = 55;
+		tv1.kuvanLaatu = "4K";
+		tv1.omistaja ="Miro";
+		
+		System.out.println(tv1.merkki);
+		tv1.startUp();
+		
+		
+		Televisio tv2 = new Televisio();
+		
+		//Annetaan arvot
+		tv1.merkki = "Toosa";
+		tv1.malli = "putkitv";
+		tv1.koko = 23;
+		tv1.kuvanLaatu = "1/10K";
+		tv1.omistaja ="Patu";
+		
+		System.out.println(tv2.merkki);
+		tv1.startUp();
 	}
 
 }
@@ -16,10 +43,12 @@ class Televisio {
 	String malli;
 	int koko;
 	String kuvanLaatu;
+	String omistaja;
 	
 	//toiminnot
-	public void Startup() {
+	public void startUp() {
 		System.out.println("TV käynnistyy");
+		omistaja() ;
 	}
 	public void shutDown() {
 		System.out.println("TV sammuu");
@@ -30,5 +59,8 @@ class Televisio {
 	}
 	public void netflix() {
 		System.out.println("Opening netflix");
+	}
+	private void omistaja() {
+		System.out.println("omistaja on "+omistaja);
 	}
 }
