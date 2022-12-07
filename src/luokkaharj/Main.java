9eedfe31c73ab4a16e8151615a7b6dbebed96e27
miropat <93606923+miropat@users.noreply.github.com>
2022-12-07 11:52:27 +0200

    first commit

---
+++ src/luokkaharj/Main.java
@@ -0,0 +1,28 @@
+package luokkaharj;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Televisio {
+	//ominaisuudet
+	String merkki;
+	String malli;
+	int koko;
+	String kuvanLaatu;
+	
+	//toiminnot
+	public void Startup() {
+		System.out.println("TV käynnistyy");
+		
+	}
+	public void shutDown() {
+		
+	}
+}
